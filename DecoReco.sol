da9c6fdcfc7712f8e3f0e2f402707454bb615111
tanweiliAtom <weili@atomionics.com>
2023-10-24 17:40:50 +0800

    Link DecoReco to use minHeap.sol. Change minHeap types from uint256 to uint16.

--- DecoReco.sol
+++ DecoReco.sol
@@ -1,18 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "./MinHeap.sol";
+
 contract DecoReco {
     mapping(address => bool) private admins;
     mapping(address => Student) private registeredStudents;
-    mapping(string => Module) private modules; // String here is module code like EE4032
-    mapping(string => mapping(address => bool)) private enrolledStudents; // String here is module code like EE4032, address is student's address
-    mapping(string => mapping(address => uint16)) private Bids; // String here is module code like EE4032, address is student's address
+    mapping(string => Module) private modules;
+    mapping(string => MinHeap) private Bids;
+    mapping(string => mapping (address => uint16)) BidMade;
 
     constructor() {
         admins[0x386703857E714284e154a7d937348d2d88a702D8] = true; // Wei Li
         admins[0xF3454f923316E665C16C4591D7c0B5aA2b12a201] = true; // Winnie
         admins[0xaB066AeA51c007923885d8949128325198467F67] = true; // Wraine
-        modules["EE4032"] = Module("Blockchain Engineering", "This module provides an introduction to blockchain.", 70, 0, true);
+        modules["EE4032"] = Module("Blockchain Engineering", "This module provides an introduction to blockchain.", 70, true);
+        Bids["EE4032"] = new MinHeap(70);
     }
 
     modifier onlyOwner() {
@@ -37,17 +40,20 @@ contract DecoReco {
 
     function createModule(string calldata _moduleCode, string calldata _moduleName, string calldata _moduleDescription, uint16 _maxCapacity) public onlyOwner {
         require(modules[_moduleCode].isAvailable == false, "Module already exists.");
-        modules[_moduleCode] = Module(_moduleName, _moduleDescription, _maxCapacity, 0, true);
+        modules[_moduleCode] = Module(_moduleName, _moduleDescription, _maxCapacity, true);
+        Bids[_moduleCode] = new MinHeap(_maxCapacity);
     }
 
     function updateModule(string calldata _moduleCode, string calldata _moduleName, string calldata _moduleDescription, uint16 _maxCapacity) public onlyOwner {
         require(modules[_moduleCode].isAvailable == true, "Module does not exists.");
-        modules[_moduleCode] = Module(_moduleName, _moduleDescription, _maxCapacity, 0, true);
+        modules[_moduleCode] = Module(_moduleName, _moduleDescription, _maxCapacity, true);
+        Bids[_moduleCode] = new MinHeap(_maxCapacity);
     }
 
     function deleteModule(string calldata _moduleCode) public onlyOwner {
         require(modules[_moduleCode].isAvailable == true, "Module does not exist or has been deleted before.");
         modules[_moduleCode].isAvailable = false;
+        Bids[_moduleCode] = new MinHeap(0);
     }
 
     function viewModule(string calldata _moduleCode) public view returns (string memory, string memory, uint16) {
@@ -59,16 +65,17 @@ contract DecoReco {
         require(_amount > 0, "Bid amount must be more than 0.");
         require(modules[_moduleCode].isAvailable == true, "Cannot bid for a non existent module.");
         require(registeredStudents[msg.sender].eDollars > _amount, "Your bid amount is more than your current remaining eDollars.");
-        Bids[_moduleCode][msg.sender] = _amount;
+        Bids[_moduleCode].insert(_amount, msg.sender);
         registeredStudents[msg.sender].eDollars -= _amount;
     }
 
     function withdrawBidForModule(string calldata _moduleCode) public onlyRegisteredStudents() {
-        require(Bids[_moduleCode][msg.sender] > 0, "You do not have a bid for the requested module.");
-        registeredStudents[msg.sender].eDollars += Bids[_moduleCode][msg.sender];
-        Bids[_moduleCode][msg.sender] = 0;
+        require(BidMade[_moduleCode][msg.sender] > 0, "You do not have a bid for the requested module.");
+        registeredStudents[msg.sender].eDollars += BidMade[_moduleCode][msg.sender];
+        BidMade[_moduleCode][msg.sender] = 0;
+        Bids[_moduleCode].removeByAddress(msg.sender);
     }
-    
+
     struct Student {
         address studentAddress;
         uint16 eDollars;
@@ -79,7 +86,6 @@ contract DecoReco {
         string moduleName;
         string moduleDescription;
         uint16 maxCapacity;
-        uint16 currentCapacity;
         bool isAvailable;
     }
 
--- MinHeap.sol
+++ MinHeap.sol
@@ -8,9 +8,9 @@ contract MinHeap {
     }
 
     Bid[] private minHeap;
-    uint256 maxCapacity;
+    uint16 maxCapacity;
 
-    constructor(uint256 _maxCapacity) {
+    constructor(uint16 _maxCapacity) {
         maxCapacity = _maxCapacity;
     }
 
@@ -26,14 +26,14 @@ contract MinHeap {
             extractMin();
         }
         minHeap.push(Bid(_studentAddress, _amount));
-        uint256 currentIndex = minHeap.length - 1;
+        uint16 currentIndex = (uint16)(minHeap.length) - 1;
         bubbleUp(currentIndex);
     }
 
     function extractMin() public returns (uint16) {
         require(minHeap.length > 0, "Heap is empty");
         uint16 minValue = minHeap[0].amount;
-        uint256 lastIndex = minHeap.length - 1;
+        uint16 lastIndex = (uint16)(minHeap.length) - 1;
         minHeap[0] = minHeap[lastIndex];
         minHeap.pop();
         bubbleDown(0);
@@ -41,24 +41,24 @@ contract MinHeap {
     }
 
     function removeByAddress(address addressToRemove) public {
-        uint256 indexToRemove = minHeap.length;
-        for (uint256 i = 0; i < minHeap.length; i++) {
+        uint16 indexToRemove = (uint16)(minHeap.length);
+        for (uint16 i = 0; i < minHeap.length; i++) {
             if (minHeap[i].studentAddress == addressToRemove) {
                 indexToRemove = i;
                 break;
             }
         }
         if (indexToRemove != minHeap.length) {
-            uint256 lastIndex = minHeap.length - 1;
+            uint16 lastIndex = (uint16)(minHeap.length) - 1;
             minHeap[indexToRemove] = minHeap[lastIndex];
             minHeap.pop();
             bubbleDown(indexToRemove);
         }
     }
 
-    function bubbleUp(uint256 index) private {
+    function bubbleUp(uint16 index) private {
         while (index > 0) {
-            uint256 parentIndex = (index - 1) / 2;
+            uint16 parentIndex = (index - 1) / 2;
             if (minHeap[index].amount < minHeap[parentIndex].amount) {
                 Bid memory temp = minHeap[index];
                 minHeap[index] = minHeap[parentIndex];
@@ -70,10 +70,10 @@ contract MinHeap {
         }
     }
 
-    function bubbleDown(uint256 index) private {
-        uint256 leftChildIndex;
-        uint256 rightChildIndex;
-        uint256 smallestChildIndex;
+    function bubbleDown(uint16 index) private {
+        uint16 leftChildIndex;
+        uint16 rightChildIndex;
+        uint16 smallestChildIndex;
 
         while (true) {
             leftChildIndex = 2 * index + 1;
